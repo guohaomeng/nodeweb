b727396d3470ce8887eca6e3d084bc20212fc110
guohaomeng <3020721699@qq.com>
2019-11-05 21:49:26 +0800

    add file

---
+++ sqltest.js
@@ -0,0 +1,82 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'nodeweb',
+    password: 'mWaGpneNWYPHrcpY',
+    database: 'nodeweb'
+
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+
+    function find() {
+    var sql = 'SELECT * FROM websites';
+    //查
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+    });
+
+    function adddata() {
+        var addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
+        var addSqlParams = ['菜鸟工具', 'https://c.runoob.com', '23453', 'CN'];
+        //增
+        connection.query(addSql, addSqlParams, function (err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                return;
+            }
+
+            console.log('--------------------------INSERT----------------------------');
+            //console.log('INSERT ID:',result.insertId);        
+            console.log('INSERT ID:', result);
+            console.log('-----------------------------------------------------------------\n\n');
+        });
+    }
+
+    function updatedata() {
+        var modSql = 'UPDATE websites SET name = ?,url = ? WHERE Id = ?';
+        var modSqlParams = ['菜鸟移动站', 'https://m.runoob.com', 6];
+        //改
+        connection.query(modSql, modSqlParams, function (err, result) {
+            if (err) {
+                console.log('[UPDATE ERROR] - ', err.message);
+                return;
+            }
+            console.log('--------------------------UPDATE----------------------------');
+            console.log('UPDATE affectedRows', result.affectedRows);
+            console.log('-----------------------------------------------------------------\n\n');
+        });
+    }
+    function deldata() {
+        var delSql = 'DELETE FROM websites where id=6';
+        //删
+        connection.query(delSql, function (err, result) {
+            if (err) {
+                console.log('[DELETE ERROR] - ', err.message);
+                return;
+            }
+
+            console.log('--------------------------DELETE----------------------------');
+            console.log('DELETE affectedRows', result.affectedRows);
+            console.log('-----------------------------------------------------------------\n\n');
+        });
+    }
+
+
+    connection.end();
+
+}
